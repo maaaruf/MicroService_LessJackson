6525a4b0a03b8ca7689cdf2f064862442d468232
maaaruf <marufbdonline@gmail.com>
2023-08-07 15:06:11 +0600

    Added automapper and profiles

---
+++ CommandsService/Profiles/CommandsProfile.cs
@@ -0,0 +1,16 @@
+using AutoMapper;
+using CommandsService.Dtos;
+using CommandsService.Models;
+
+namespace CommandsService.Profiles;
+
+public class CommandsProfile : Profile
+{
+    public CommandsProfile()
+    {
+        // Source -> Target
+        CreateMap<Command, CommandReadDto>();
+        CreateMap<CommandCreateDto, Command>();
+        CreateMap<Platform, PlatformReadDto>();
+    }
+}
