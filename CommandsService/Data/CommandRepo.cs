644b7f5b0f7a4b52e693491e1dd040ed98ea948a
maaaruf <marufbdonline@gmail.com>
2023-08-08 11:08:09 +0600

    CHeck if external platform exists

--- CommandsService/Data/CommandRepo.cs
+++ CommandsService/Data/CommandRepo.cs
@@ -50,6 +50,11 @@ public class CommandRepo : ICommandRepo
         return _context.Platforms.Any(p => p.Id == platformId);
     }
 
+    public bool ExternalPlatformExists(int externalPlatformId)
+    {
+        return _context.Platforms.Any(p => p.ExternalID == externalPlatformId);
+    }
+
     public bool SaveChanges()
     {
         return _context.SaveChanges() >= 0;
